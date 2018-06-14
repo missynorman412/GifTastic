557eb0afbaf17d114d61d04df145abca316c0c5e
Missy Norman <missy.norman412@gmail.com>
2018-06-14 17:16:12 -0400

    final product

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,20 +17,17 @@ function displayGifs() {
             
             var rating = results[i].rating;
             var animalDiv = $("<div class='col-md-4'>");
-            //var animated = results[i].images.fixed_height;
-            //var image = results[i].url;
-            
-            
             var still = results[i].images.fixed_height_still.url;
             var animated = results[i].images.fixed_height.url;
-            var state = still;
-            // Creating and storing an image tag
             var image = $("<img>");
             var p = $("<p>").text("Rating: " + rating);
             // Setting the Image src attribute to imageUrl
             image.attr("src", still);
+            image.attr("data-state", "still");
+            image.attr("data-still", still);
             image.attr("alt", "image");
             image.attr("data-animate", animated);
+            image.addClass("Giphy");
             animalDiv.append(p);
             //images.append(image);
   
@@ -41,25 +38,23 @@ function displayGifs() {
 }
 
 $(".btn").on("click", function (event) {
-    console.log('adding');
     event.preventDefault();
-    var newSearch = $(".form-control").val();
-    console.log(newSearch);
+    var newSearch = $(".form-control").val().trim();
     gifArray.push(newSearch);
-    console.log(gifArray);
     $("#Input").val("");
     displayButtons();
 });
 
 function displayButtons() {
-    console.log('adding2');
     $("animal-buttons").empty();
     for (var i = 0; i < gifArray.length; i++) {
         var a = $("<button>");
         a.attr("id", "animal");
+        a.addClass(".Giphy");
         a.attr("data-search", gifArray[i]);
         a.text(gifArray[i]);
         $("#animal-buttons").append(a);
+        a.attr("data-state", "still");
     }
 }
 
@@ -68,11 +63,16 @@ $(document).on("click", "#animal", displayGifs);
 $(document).on("click", ".Giphy", pausePlayGifs);
 
 function pausePlayGifs() {
+    
     var state = $(this).attr("data-state");
+    console.log(state);
     if (state === "still") {
+    
+        //this is supposed to animate gifs that are still
         $(this).attr("src", $(this).attr("data-animate"));
-        $(this).attr("data-state", "still");
+        $(this).attr("data-state", "animate");
     } else {
+        //and still gifs that are animated.
         $(this).attr("src", $(this).attr("data-still"));
         $(this).attr("data-state", "still");
     }
